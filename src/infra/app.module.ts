fc97e94e382a68c2ef2a74a61c2a9ec365928fba
nicolasmacedoo <nicolas.conceicao01@gmail.com>
2025-03-04 01:19:27 -0300

    :truck: refactor(app): move AppModule and main entry point for application initialization

---
+++ src/infra/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env/env'
+import { AuthModule } from './auth/auth.module'
+import { EnvModule } from './env/env.module'
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: env => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+    EnvModule,
+  ],
+})
+export class AppModule {}
