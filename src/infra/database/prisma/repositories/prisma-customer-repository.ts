b72edc281c9c47b7f28d3350950990e87f8800f3
nicolasmacedoo <nicolas.conceicao01@gmail.com>
2025-03-04 01:41:27 -0300

    :sparkles: feat(repositories): add PrismaCustomerRepository and PrismaUsersRepository for customer and user data handling

---
+++ src/infra/database/prisma/repositories/prisma-customer-repository.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common'
+import type { PrismaService } from '../prisma.service'
+import { CustomersRepository } from '@/domain/ecommerce/application/repositories/customers-repository'
+import type { Customer } from '@/domain/ecommerce/enterprise/entities/customer'
+import { PrismaCustomerMapper } from '../mappers/prisma-customer-mapper'
+
+@Injectable()
+export class PrismaCustomerRepository implements CustomersRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findByUserId(userId: string): Promise<Customer | null> {
+    const customer = await this.prisma.customer.findUnique({
+      where: {
+        userId,
+      },
+    })
+
+    if (!customer) {
+      return null
+    }
+
+    return PrismaCustomerMapper.toDomain(customer)
+  }
+
+  async create(customer: Customer): Promise<void> {
+    const data = PrismaCustomerMapper.toPersistence(customer)
+
+    await this.prisma.customer.create({
+      data,
+    })
+  }
+}
---
+++ src/infra/database/prisma/repositories/prisma-users-repository.ts
@@ -0,0 +1,32 @@
+import type { UsersRepository } from '@/domain/ecommerce/application/repositories/users-repository'
+import type { User } from '@/domain/ecommerce/enterprise/entities/user'
+import { Injectable } from '@nestjs/common'
+import type { PrismaService } from '../prisma.service'
+import { PrismaUserMapper } from '../mappers/prisma-user-mapper'
+
+@Injectable()
+export class PrismaUsersRepository implements UsersRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (!user) {
+      return null
+    }
+
+    return PrismaUserMapper.toDomain(user)
+  }
+
+  async create(user: User): Promise<void> {
+    const data = PrismaUserMapper.toPersistence(user)
+
+    await this.prisma.user.create({
+      data,
+    })
+  }
+}
