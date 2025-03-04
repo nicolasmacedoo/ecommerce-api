9b6ded795b656f65e2c7ac15be9e0e500d1e3da8
nicolasmacedoo <nicolas.conceicao01@gmail.com>
2025-03-04 01:03:02 -0300

    :sparkles: feat(app): integrate ConfigModule and AuthModule into the main application module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './infra/env/env'
+import { AuthModule } from './infra/auth/auth.module'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: env => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
