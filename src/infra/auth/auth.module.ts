69ccfdde31ae77ae4b9834f272c60fd14d210146
nicolasmacedoo <nicolas.conceicao01@gmail.com>
2025-03-04 01:02:37 -0300

    :sparkles: feat(auth): implement JWT authentication module with guards and decorators

---
+++ src/infra/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
+import { EnvService } from '../env/env.service'
+import { EnvModule } from '../env/env.module'
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [EnvModule],
+      inject: [EnvService],
+      global: true,
+      useFactory: async (env: EnvService) => ({
+        secret: env.get('JWT_SECRET'),
+        signOptions: { expiresIn: '60s', algorithm: 'HS256' },
+      }),
+    }),
+  ],
+  providers: [EnvService],
+})
+export class AuthModule {}
---
+++ src/infra/auth/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common'
+import { PassportStrategy } from '@nestjs/passport'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { z } from 'zod'
+import type { EnvService } from '../env/env.service'
+
+const tokenPayloadSchema = z.object({
+  sub: z.string().uuid(),
+})
+
+export type UserPayload = z.infer<typeof tokenPayloadSchema>
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(configService: EnvService) {
+    const jwtSecret = configService.get('JWT_SECRET')
+
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtSecret,
+      algorithms: ['HS256'],
+    })
+  }
+
+  async validate(payload: UserPayload) {
+    return tokenPayloadSchema.parse(payload)
+  }
+}
