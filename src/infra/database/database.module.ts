0ee191bda2d2ac0ed65eb5f9362b65cfa329001c
nicolasmacedoo <nicolas.conceicao01@gmail.com>
2025-03-04 01:43:22 -0300

    :sparkles: feat(database): register UsersRepository and CustomersRepository in DatabaseModule for dependency injection

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
+import { UsersRepository } from '@/domain/ecommerce/application/repositories/users-repository'
+import { PrismaUsersRepository } from './prisma/repositories/prisma-users-repository'
+import { CustomersRepository } from '@/domain/ecommerce/application/repositories/customers-repository'
+import { PrismaCustomersRepository } from './prisma/repositories/prisma-customer-repository'
 
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: UsersRepository,
+      useClass: PrismaUsersRepository,
+    },
+    {
+      provide: CustomersRepository,
+      useClass: PrismaCustomersRepository,
+    },
+  ],
+  exports: [PrismaService, UsersRepository, CustomersRepository],
 })
 export class DatabaseModule {}
