941ee834318f4f8bb4e892f163b42fe8be257536
nicolasmacedoo <nicolas.conceicao01@gmail.com>
2025-03-04 22:46:31 -0300

    :books: docs(api): integrate Swagger for API documentation and set global prefix

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,11 +1,27 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { EnvService } from './env/env.service'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { patchNestJsSwagger } from 'nestjs-zod'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   const envService = app.get(EnvService)
+
+  const config = new DocumentBuilder()
+    .setTitle('Ecommerce API')
+    .setDescription('REST API for a simplified e-commerce management system')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+
+  patchNestJsSwagger()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, documentFactory())
+
+  app.setGlobalPrefix('api')
 
   const PORT = envService.get('PORT')
 
