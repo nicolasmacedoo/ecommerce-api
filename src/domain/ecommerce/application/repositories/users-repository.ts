66d814ea4cdf5a73342114c3823053635abf5781
nicolasmacedoo <nicolas.conceicao01@gmail.com>
2025-03-10 11:34:32 -0300

    :sparkles: feat(users): update findByEmail method to return UserWithRoleAndPermissions and add UserWithRoleAndPermissions value object

--- src/domain/ecommerce/application/repositories/users-repository.ts
+++ src/domain/ecommerce/application/repositories/users-repository.ts
@@ -1,6 +1,9 @@
 import type { User } from '../../enterprise/entities/user'
+import type { UserWithRoleAndPermissions } from '../../enterprise/entities/value-objects/user-with-role-and-permissions'
 
 export abstract class UsersRepository {
-  abstract findByEmail(email: string): Promise<User | null>
+  abstract findByEmail(
+    email: string
+  ): Promise<UserWithRoleAndPermissions | null>
   abstract create(user: User): Promise<void>
 }
---
+++ src/domain/ecommerce/enterprise/entities/value-objects/user-with-role-and-permissions.ts
@@ -0,0 +1,57 @@
+import { ValueObject } from '@/core/entities/value-object'
+
+interface Permissions {
+  id: string
+  name: string
+  description: string | null
+}
+
+interface UserWithRoleAndPermissionsProps {
+  id: string
+  role: string
+  permissions: Permissions[]
+  email: string
+  name: string
+  password: string
+  createdAt: Date
+  updatedAt?: Date | null
+}
+
+export class UserWithRoleAndPermissions extends ValueObject<UserWithRoleAndPermissionsProps> {
+  static create(
+    props: UserWithRoleAndPermissionsProps
+  ): UserWithRoleAndPermissions {
+    return new UserWithRoleAndPermissions(props)
+  }
+
+  get id(): string {
+    return this.props.id
+  }
+
+  get role(): string {
+    return this.props.role
+  }
+
+  get permissions(): Permissions[] {
+    return this.props.permissions
+  }
+
+  get email(): string {
+    return this.props.email
+  }
+
+  get name(): string {
+    return this.props.name
+  }
+  get password(): string {
+    return this.props.password
+  }
+
+  get createdAt(): Date {
+    return this.props.createdAt
+  }
+
+  get updatedAt(): Date | null | undefined {
+    return this.props.updatedAt
+  }
+}
